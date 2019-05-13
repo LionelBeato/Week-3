392d5b927aeef6cceebd5df1ef5f27eb51b75171
leo <lionel.beato@gmail.com>
2019-05-13 14:37:38 -0400

    hopefully this works lelel

---
+++ ts.js
@@ -0,0 +1,11 @@
+var score_1 = 0;
+var score_2 = 0;
+
+var playerName_1 = "Mike"
+var playerName_2 = "Dennis"
+var weapons = ['rock' , 'paper', 'scissors'];
+var weaponOfChoice = weapons[parseInt(Math.random()*weapons.length) %3];
+var weaponOfChoice_2 = weapons[parseInt(Math.random()*weapons.length) %3];
+
+console.log(playerName_1+" played "+weaponOfChoice)
+console.log(playerName_1+" played "+weaponOfChoice)
