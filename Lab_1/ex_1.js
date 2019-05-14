0abc8ca5f53fb0ddf6c7c6cf8e319b72082e5051
leo <lionel.beato@gmail.com>
2019-05-14 16:25:58 -0400

    finished excercise

--- Lab_1/ex_1.js
+++ Lab_1/ex_1.js
@@ -1,3 +1,28 @@
-var groceries = ['pop tarts', 'ramen noodles', 'chips', 'salsa', 'coffee'];
+var list = ['pop tarts', 'ramen noodles', 'chips', 'salsa', 'coffee'];
 
-console.log(groceries)
+var addGroc = list.push('fruit loops');
+
+list[4] = 'fair trade coffee';
+list[2] = 'rice';
+list[3] = 'beans';
+
+var cart = [];
+var poppedItems = list.pop();
+var shiftedItems = list.shift();
+
+cart.push(poppedItems, shiftedItems);
+
+console.log(list)
+console.log(cart)
+
+//something that takes items from one array and puts it into another
+
+while (list.length > 0){
+    var autoItems = list.shift();
+    cart.push(autoItems);
+    console.log('\n', cart)
+}
+
+
+
+
