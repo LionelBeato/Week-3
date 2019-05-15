dd240031ff528d420ce91136bcff46614631ed44
leo <lionel.beato@gmail.com>
2019-05-15 16:33:03 -0400

    zipped up lab 1

---
+++ Lab_1/email_app_LB.js
@@ -0,0 +1,18 @@
+var appData = {
+	name: 'Gmail',
+	mailboxes: [
+		'inbox',
+		'starred',
+        'sent',
+        'important',
+        'promotions',
+        'draft',
+	],
+	contacts: [
+		{name: 'Stephanie', lastMessage: "I'm open on Friday"},
+		{name: 'Grandpa', lastMessage: "Make $10,000 Online With This One Weird Trick!"}
+	]
+
+}
+
+console.log(appData.mailboxes)
